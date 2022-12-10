a94bbb8780801a07bc719941c8c0f116cecbb54e
izhichao <60996631+izhichao@users.noreply.github.com>
2022-12-10 20:01:27 +0800

    feat: 封装发送邮件函数

--- utils/email.js
+++ utils/email.js
@@ -1,9 +1,19 @@
 const nodemailer = require('nodemailer');
 
-module.exports = nodemailer.createTransport({
+const transporter = nodemailer.createTransport({
   host: 'smtp.exmail.qq.com',
   port: '465',
   secureConnection: true,
   secure: true,
   auth: { user: process.env.EMAIL_USERNAME, pass: process.env.EMAIL_PASSWORD }
 });
+
+function sendMail({ email, title, text }) {
+  transporter.sendMail({
+    subject: `[互助圈] ${title}`,
+    from: `互助圈 ${process.env.EMAIL_USERNAME}`,
+    to: `${email}`,
+    text
+  });
+}
+module.exports = sendMail;
