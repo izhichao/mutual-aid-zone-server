a913dac59bf72881cf96d87eda3d27e49cc20107
izhichao <60996631+izhichao@users.noreply.github.com>
2023-03-10 18:50:50 +0800

    feat: 新增查询订单详情与编辑订单功能

--- controller/OrderController.js
+++ controller/OrderController.js
@@ -16,9 +16,21 @@ class OrderController {
     return orders;
   }
 
+  static async getOrder(query) {
+    const { _id } = query;
+    const order = await Order.findById(_id).populate('user', 'username').lean();
+    order.user = order.user.username;
+    return order;
+  }
+
   static async editOrder(body) {
     const { _id, address, express } = body;
-
+    let updateObj = { address };
+    if (express) {
+      updateObj = { address, express, status: 1 };
+    }
+    await Order.findByIdAndUpdate(_id, updateObj);
+    return '编辑成功';
   }
 
   static async finishOrder(body) {
--- routes/order.js
+++ routes/order.js
@@ -10,6 +10,18 @@ router.get('/', async (ctx, next) => {
   ctx.body = new SuccessModel(data);
 });
 
+router.get('/detail', async (ctx, next) => {
+  const query = ctx.query;
+  const data = await OrderController.getOrder(query);
+  ctx.body = new SuccessModel(data);
+});
+
+router.post('/edit', async (ctx, next) => {
+  const body = ctx.request.body;
+  const data = await OrderController.editOrder(body);
+  ctx.body = new SuccessModel(data);
+});
+
 router.post('/finish', async (ctx, next) => {
   const body = ctx.request.body;
   const data = await OrderController.finishOrder(body);
