7d73df74bccb2cf5a8ec868fea59232d57fe2dd7
izhichao <60996631+izhichao@users.noreply.github.com>
2024-06-07 12:32:09 +0800

    feat: 支持自定义 MongoDB 数据库名与认证源

--- utils/db.js
+++ utils/db.js
@@ -3,14 +3,15 @@ const dbIp = process.env.DB_IP || '127.0.0.1';
 const dbPort = process.env.DB_PORT || '27017';
 const dbUsername = process.env.DB_USERNAME;
 const dbPassword = process.env.DB_PASSWORD;
+const dbName = process.env.DB_NAME || 'zone';
+const dbAuthSource = process.env.DB_AUTHSOURCE || 'zone';
 
 let url = `mongodb://${dbIp}:${dbPort}`;
 if (dbUsername && dbPassword) {
   url = `mongodb://${dbUsername}:${dbPassword}@${dbIp}:${dbPort}`;
 }
-console.log(url);
-const dbName = 'zone';
-mongoose.connect(`${url}/${dbName}`);
+
+mongoose.connect(`${url}/${dbName}?authSource=${dbAuthSource}`);
 
 const db = mongoose.connection;
 
